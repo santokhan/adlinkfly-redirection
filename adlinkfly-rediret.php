2853f5b9765d4ff2120df2a5170e008d73649ca4
Santo Khan <santokhan1999@gmail.com>
2024-08-20 13:56:46 +0600

    Update adlinkfly-rediret.php

--- adlinkfly-rediret.php
+++ adlinkfly-rediret.php
@@ -1,20 +1,25 @@
 <?php
 
-if($_POST['ref']) { 
+if ($_POST['ref']) {
+    // Maybe do not have any route for https://teraboxfan.com/alias POST method
 } else {
-$ref=$_SERVER[HTTP_REFERER];
-$do = parse_url($ref);
-$refer = $do['host'];
+    // For GET requests
+    $ref = $_SERVER[HTTP_REFERER];
+    $do = parse_url($ref);
+    $refer = $do['host'];
 
-if($refer=="ertyf.xyz" ) {
+    if ($refer == "ertyf.xyz") {
+        ?>
+
+        <!--=== PAST YOUR view_banner.cpt CODE HERE ===--!>
+
+<?php
+    } else {
+
+        header("Location: https://ertyf.xyz/?adlinkfly=$link->alias", true, 307);
+
+        exit();
+
+    }
+}
 ?>
-
-<!--=== PAST YOUR view_banner.cpt CODE HERE ===--!>
-
-<?php } else {
-
- header("Location: https://ertyf.xyz/?adlinkfly=$link->alias", true, 307);
-
-exit(); 
-
-} } ?>
